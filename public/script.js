27c320ea96d5d033d37c6873489323e210a19141
Jake7208 <jake7208@gmail.com>
2023-07-19 10:18:41 -0600

    fix .env not appear

---
+++ public/script.js
@@ -0,0 +1,21 @@
+fetch("http://localhost:4000/api/getPath")
+  .then((response) => response.json())
+  .then((objectData) => {
+    const paths = objectData.path; // Access the 'path' property from the objectData
+
+    const listElement = document.createElement("li"); // Create a new <ul> element
+
+    paths.forEach((path) => {
+      const listItem = document.createElement("ul");
+      const imgElement = document.createElement("img"); // Create a new <img> element
+
+      imgElement.src = `.././images/${path}`; // Set the 'src' attribute dynamically
+      listItem.appendChild(imgElement); // Append the <img> element to the <li> element
+      listElement.appendChild(listItem); // Append the <li> element to the <ul> element
+    });
+
+    document.body.appendChild(listElement); // Append the <ul> element to the document body
+  })
+  .catch((error) => {
+    console.error(error);
+  });
